2c7fd289c55472b420bde78ec873a479f099c597
GBaykov <73140481+GBaykov@users.noreply.github.com>
2021-03-18 21:22:36 +0300

    Update vigenere-cipher.js

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,61 @@
 const CustomError = require("../extensions/custom-error");
+let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(Boo) {
+    if (Boo === undefined) {
+      this.Boo = true;
+    } else {
+      this.Boo = Boo;
+    }
   }
-}
+  encrypt(str, key) {
+    str = str.toUpperCase().split('');
+    key = key.toUpperCase().split('');
+      if (!str || !key) {throw Error;}
+     let encrypted = [];
+     let sum = [];
 
+for (let i = 0; i<str.length; i++) {
+  if (alphabet.includes(str[i])) {
+    if (str.length>key.length) {key.push(key[i])}
+      if (alphabet.indexOf(str[i])+ alphabet.indexOf(key[i]) >= 26) {
+        sum.push(alphabet.indexOf(str[i])+ alphabet.indexOf(key[i]) - 26)
+      } else {sum.push(alphabet.indexOf(str[i])+ alphabet.indexOf(key[i]))}
+      encrypted.push(alphabet[sum[i]])
+  } else {
+    sum.push(str[i]);
+    encrypted.push(str[i]);
+    key.splice(i,0,' ');
+  }
+} return this.Boo ? encrypted.join('') : encrypted.reverse().join('');
+  }
+
+  decrypt(str, key) {
+   str = str.toUpperCase().split('');
+   key = key.toUpperCase().split('');
+   if (!str || !key) {throw Error;}
+   let decrypted = [];
+     let sum = [];
+   for (let i = 0; i<str.length ; i++) {
+     if (alphabet.includes(str[i])) {
+       if (str.length > key.length ) {
+         key.push(key[i])
+       }
+       if (alphabet.indexOf(str[i])< alphabet.indexOf(key[i])) {
+         sum.push((alphabet.indexOf(str[i]) - alphabet.indexOf(key[i])) + 26);
+       } else {
+         sum.push(alphabet.indexOf(str[i])- alphabet.indexOf(key[i]));
+       }
+       decrypted.push(alphabet[sum[i]])
+     }
+     else {
+       sum.push(str[i]);
+       decrypted.push(str[i]);
+       key.splice(i,0,' ');
+     }
+   } return this.Boo ? decrypted.join('') : decrypted.reverse().join('');
+ }
+}
 module.exports = VigenereCipheringMachine;
+

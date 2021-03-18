870e681929ec5465ba0aa761559edfe449529bd9
GBaykov <73140481+GBaykov@users.noreply.github.com>
2021-03-18 23:06:02 +0300

    fix: fix mistake in position

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,9 +4,9 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity ) {
-  sampleActivity = Number(sampleActivity);
+  
 if (typeof sampleActivity != 'string') return false;
-
+sampleActivity = Number(sampleActivity);
   if(sampleActivity > 0 && sampleActivity <= 15  ){
     let k = 0.693 / HALF_LIFE_PERIOD;
     let t = Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / k);

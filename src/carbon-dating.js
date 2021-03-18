892cd1145a7e36575d0f047e435af43c3133a06d
GBaykov <73140481+GBaykov@users.noreply.github.com>
2021-03-18 22:56:31 +0300

    fix: code
    someone else's code to check the performance of tests

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,15 +3,9 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(sampleActivity ) {
-  sampleActivity = parseInt(sampleActivity);
-  if(sampleActivity <= 0 || typeof(sampleActivity) !== Number|| sampleActivity > 15  ){
-    return false;
-  } else {
-    let k = 0.693 / HALF_LIFE_PERIOD;
-    let t = Math.log(MODERN_ACTIVITY / sampleActivity) / k;
-
-      return Math.ceil(t);
+module.exports = function dateSample(sampleActivity) {
+  if (isNaN(parseInt(sampleActivity)) || parseFloat(sampleActivity) > MODERN_ACTIVITY || parseInt(sampleActivity) <= 0 || typeof sampleActivity !== 'string') {
+    return false
   }
-
-};
+  return Math.ceil(Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity)) / (Math.LN2 / HALF_LIFE_PERIOD))
+}
